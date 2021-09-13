5d79ff9e20878d171d3e05b3be62f04cdce6c62e
Fernando de Paula <dipaula018@gmail.com>
2021-09-13 20:31:34 -0300

    fix: styles

--- src/screens/BookmarksList.js
+++ src/screens/BookmarksList.js
@@ -117,12 +117,3 @@ export default function BookmarksList() {
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
