b7f165af80558273bd42d3db99b2e5ce94fb844c
Fernando de Paula <fernando.maciel@q2pay.com.br>
2021-09-13 18:05:54 -0300

    feat: finished

--- src/navigation/RootNavigator.js
+++ src/navigation/RootNavigator.js
@@ -8,3 +8,49 @@ import BooksList from '../screens/BooksList';
 import BookmarksList from '../screens/BookmarksList';
 
 const Tab = createBottomTabNavigator();
+
+const tabBarOptions = {
+  showLabel: false,
+  inactiveTintColor: '#2D3038',
+  activeTintColor: '#FFFFFF',
+  style: {
+    height: '10%',
+    backgroundColor: '#1E1B26'
+  }
+};
+
+const screenOptions = (route, color) => {
+  let iconName;
+
+  switch (route.name) {
+    case 'BooksList':
+      iconName = 'view-dashboard';
+      break;
+    case 'BookmarksList':
+      iconName = 'bookmark-multiple-outline';
+      break;
+    default:
+      break;
+  }
+
+  return <MaterialCommunityIcons name={iconName} color={color} size={24} />;
+};
+
+const RootNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName='BooksList'
+        tabBarOptions={tabBarOptions}
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color }) => screenOptions(route, color)
+        })}
+      >
+        <Tab.Screen name='BooksList' component={BooksList} />
+        <Tab.Screen name='BookmarksList' component={BookmarksList} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default RootNavigator;
---
+++ src/redux/store.js
@@ -0,0 +1,20 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { persistStore, persistReducer } from 'redux-persist';
+
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+  whitelist: ['bookmarks']
+};
+
+import booksReducer from './reducers';
+
+const rootReducer = combineReducers({
+  booksReducer: persistReducer(persistConfig, booksReducer)
+});
+
+
+export const store = createStore(rootReducer, applyMiddleware(thunk));
+export const persistor = persistStore(store);
